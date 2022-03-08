7dde509759b157bbc6110bbfb50a27c1a77321f8
Joseph Robinson <smartech56@gmail.com>
2022-03-08 01:53:36 -0700

    Fixed fatal error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
 const config = require('./config.json');
 const client = new Client({
 	disableEveryone: true,
@@ -48,6 +48,28 @@ client.on('interactionCreate', async interaction => {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
+});
+
+/* Client when detects a new member join */
+client.on('guildMemberAdd', async (member) => {
+	const guild = member.guild;
+	// exclude channel search in all other guilds
+	const channel = guild.channels.cache.find((c) => c.name === 'welcome');
+	const welcome = new MessageEmbed()
+		.setTitle('New User Has Joined!')
+		.setDescription(`Welcome To Our Server ${member.user}! We are happy to have you in this server! You are member number ${guild.memberCount} btw!`)
+		.setColor('#2F3136')
+		.setThumbnail(
+			member.displayAvatarURL({
+				dynamic: true,
+			}),
+		)
+		.setTimestamp()
+		.setFooter({ text: 'JosephWorks Discord Bot', iconURL: 'https://i.imgur.com/wSTFkRM.png' });
+	if (!channel) return console.log('You do not have a channel called welcome, please make one or set the name of the channel in line 27 of the code.');
+	channel?.send({
+		embeds: [welcome],
+	});
 });
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
