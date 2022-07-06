65a93467cc0639fd218b9860b72c8797fd4f6712
Joseph Robinson <smartech56@gmail.com>
2022-07-06 01:45:39 -0700

    Fixed nitro booster check

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const fs = require('node:fs');
 const { Client, Collection, MessageEmbed } = require('discord.js');
 const MongoClient = require('mongodb').MongoClient;
@@ -66,26 +67,30 @@ client.on('guildMemberAdd', async (member) => {
 
 /* Client when detects a nitro boost */
 // TODO: Move to own file
-client.on('guildMemberUpdate', async (member) => {
-	const guild = member.guild;
+client.on('guildMemberUpdate', async (oldMember, newMember) => {
+	const guild = oldMember.guild;
 	// exclude channel search in all other guilds
 	const channel = guild.channels.cache.find((c) => c.name === 'welcome');
-	const nitro = new MessageEmbed()
-		.setTitle('New Nitro Boost!')
-		.setDescription(`${member.user} has just boosted the server!`)
-		// set color pink
-		.setColor('#ff7aff')
-		.setThumbnail(
-			member.displayAvatarURL({
-				dynamic: true,
-			}),
-		)
-		.setTimestamp()
-		.setFooter({ text: 'JosephWorks Discord Bot', iconURL: 'https://media.discordapp.net/stickers/979183132165148712.png' });
-	if (!channel) return console.log('You do not have a channel called welcome, please make one or set the name of the channel in line 27 of the code.');
-	channel?.send({
-		embeds: [nitro],
-	});
+	const hadRole = oldMember.roles.find(role => role.name === 'Nitro Booster');
+	const hasRole = newMember.roles.find(role => role.name === 'Nitro Booster');
+	if (!hadRole && hasRole) {
+		const nitro = new MessageEmbed()
+			.setTitle('New Nitro Boost!')
+			.setDescription(`${newMember.user} has just boosted the server!`)
+			// set color pink
+			.setColor('#ff7aff')
+			.setThumbnail(
+				newMember.displayAvatarURL({
+					dynamic: true,
+				}),
+			)
+			.setTimestamp()
+			.setFooter({ text: 'JosephWorks Discord Bot', iconURL: 'https://media.discordapp.net/stickers/979183132165148712.png' });
+		if (!channel) return console.log('You do not have a channel called welcome, please make one or set the name of the channel in line 27 of the code.');
+		channel?.send({
+			embeds: [nitro],
+		});
+	}
 });
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
