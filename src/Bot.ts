32936b9cf7cd5dc8ae6ee5fa8bd1509ae4c07513
Joseph Robinson <smartech56@gmail.com>
2022-08-27 00:10:53 -0700

    Removed Partials

--- src/Bot.ts
+++ src/Bot.ts
@@ -12,16 +12,7 @@ const config = require('../config.json')
 
 const client = new Client({
     // All intents are enabled by default.
-    intents: 32767,
-    partials: [
-        Partials.User,
-        Partials.Channel,
-        Partials.GuildMember,
-        Partials.Message,
-        Partials.Reaction,
-        Partials.GuildScheduledEvent,
-        Partials.ThreadMember
-    ]
+    intents: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536],
 })
 
 // Replace the uri string with your MongoDB deployment's connection string.
