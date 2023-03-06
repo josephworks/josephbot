3463d8213c8f43e855807097c43da7679cf48256
Joseph Robinson <smartech56@gmail.com>
2023-03-05 23:47:52 -0700

    Fixed formatting for Command class

--- src/handlers/Command.ts
+++ src/handlers/Command.ts
@@ -32,9 +32,20 @@ module.exports = (client: Client) => {
         body: slashCommands.map(command => command.toJSON())
     })
         .then((data: any) => {
-            console.log(color('text', `🔥 Successfully loaded ${color('variable', data.length)} slash command(s)`))
-            console.log(color('text', `🔥 Successfully loaded ${color('variable', commands.length)} command(s)`))
-        }).catch(e => {
-        console.log(e)
-    })
+            console.log(
+                color(
+                    'text',
+                    `🔥 Successfully loaded ${color('variable', data.length)} slash command(s)`
+                )
+            )
+            console.log(
+                color(
+                    'text',
+                    `🔥 Successfully loaded ${color('variable', commands.length)} command(s)`
+                )
+            )
+        })
+        .catch(e => {
+            console.log(e)
+        })
 }
