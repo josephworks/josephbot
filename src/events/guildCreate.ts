62184699fba7247ff1c46620cee5fb92a728a464
Joseph Robinson <smartech56@gmail.com>
2023-03-22 15:27:35 -0700

    Update Guild Create Event

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,4 +1,5 @@
 import { Guild } from 'discord.js'
+import UserModel from '../schemas/User'
 import GuildModel from '../schemas/Guild'
 import { BotEvent } from '../types'
 
@@ -22,6 +23,52 @@ const event: BotEvent = {
             joinedAt: Date.now()
         })
         newGuild.save()
+
+        guild.members.cache.forEach(async member => {
+            try {
+                const doc = await UserModel.findById(member.id)
+                if (!doc) {
+                    const newUser = new UserModel({
+                        _id: member.id,
+                        username: member.user.username,
+                        discriminator: member.user.discriminator,
+                        avatar: member.user.avatar,
+                        createdAt: member.user.createdAt,
+                        bot: member.user.bot,
+                        guildData: [
+                            {
+                                guildID: member.guild.id,
+                                roles: member.roles.cache.map(role => role.id),
+                                joinedAt: member.joinedAt,
+                                premium: member.premiumSince
+                            }
+                        ]
+                    })
+                    newUser.save()
+                } else {
+                    let hasData = false
+                    for (let i = 0; i < doc.guildData.length; i++) {
+                        if (doc.guildData[i].guildID === member.guild.id) {
+                            hasData = true
+                        }
+                    }
+                    if (!hasData) {
+                        doc.guildData.push({
+                            guildID: member.guild.id,
+                            roles: member.roles.cache.map(role => role.id),
+                            joinedAt: member.joinedAt!,
+                            premium: member.premiumSince!,
+                            options: {
+                                vcPing: false
+                            }
+                        })
+                    }
+                    await doc.save()
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        })
     }
 }
 
