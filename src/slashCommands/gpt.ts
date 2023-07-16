e84192ec00e8ed9ac29c207f820121fbba4f2cf7
Joseph Robinson <smartech56@gmail.com>
2023-07-16 13:29:17 -0700

    Updated to GPT 3.5

--- src/slashCommands/gpt.ts
+++ src/slashCommands/gpt.ts
@@ -3,7 +3,7 @@ import { SlashCommand } from '../types'
 import { Configuration, OpenAIApi } from 'openai'
 import OpenAIRequestModel from '../schemas/OpenAIRequest'
 
-const GPTCommand : SlashCommand = {
+const GPTCommand: SlashCommand = {
     command: new SlashCommandBuilder()
         .setName('gpt')
         .setDescription('Sends a message to the GPT-3 API.')
@@ -17,32 +17,35 @@ const GPTCommand : SlashCommand = {
     execute: async interaction => {
         const message = interaction.options.get('message')
 
+        await interaction.deferReply();
+
         const configuration = new Configuration({
+            organization: process.env.OPENAI_ORG,
             apiKey: process.env.OPENAI_API_KEY
         })
         const openai = new OpenAIApi(configuration)
 
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: message?.value as string,
-            temperature: 0.7,
-            max_tokens: 512,
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ "role": "system", "content": "You are a helpful technical assistant." }, { role: "user", content: message?.value as string }],
+            temperature: 1,
+            max_tokens: 256,
             top_p: 1,
             frequency_penalty: 0,
-            presence_penalty: 0
-        })
+            presence_penalty: 0,
+        });
 
         const OpenAIRequest = new OpenAIRequestModel({
             user: interaction.member!.user.id,
             guild: interaction.guild!.id,
             channel: interaction.channel!.id,
             question: message?.value as string,
-            answer: response.data.choices[0].text,
+            answer: response.data.choices[0].message?.content,
             date: new Date()
         })
         OpenAIRequest.save()
 
-        await interaction.reply(interaction.member!.user.username + ' asked ' + message?.value + '\n' + response.data.choices[0].text)
+        await interaction.editReply({ content: interaction.member!.user.username + ' asked ' + message?.value + '\n' + response.data.choices[0].message!.content! })
     },
     cooldown: 10
 }
