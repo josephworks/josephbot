32f959f7ab6fe19507261a1412c455dd9d7d7c8e
Joseph Robinson <smartech56@gmail.com>
2026-03-12 01:55:11 -0700

    Fix GPT crash bug

--- src/slashCommands/gpt.ts
+++ src/slashCommands/gpt.ts
@@ -30,7 +30,7 @@ const GPTCommand: SlashCommand = {
                 { role: 'user', content: message?.value as string }
             ],
             temperature: 1,
-            max_tokens: 512,
+            max_completion_tokens: 1024,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0
